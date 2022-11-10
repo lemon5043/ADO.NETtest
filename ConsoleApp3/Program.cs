5892bf905d1c1f9043c4bfa0a43ac8bf07ff69d6
伯翰 侯 <john860703@gmail.com>
2022-11-10 13:54:38 +0800

    use dll to classify code

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using iSpan.Utility;
 
 namespace ConsoleApp3
 {
@@ -16,25 +18,28 @@ namespace ConsoleApp3
 			string sql = @"INSERT INTO News
 						(Title, vContent, ModifyTime)
 						VALUES(@Title, @vContent, getdate())";
+			var dbHelper = new SqlDbHelper("default");
 
 			#region 使用 using
 
-			using (var conn = new SqlConnection(connString))
-			{
+			//using (var conn = new SqlConnection(connString))
+			//{
 				try
 				{
-					SqlCommand cmd = new SqlCommand(sql, conn);
-					conn.Open();
+					//SqlCommand cmd = new SqlCommand(sql, conn);
+					//conn.Open();
 
 					SqlParameter titleParam = new SqlParameter("@Title", SqlDbType.NVarChar, 50)
 						{ Value = "Greeting" };
 
-					SqlParameter contentParameter = new SqlParameter("@vcontent", SqlDbType.NVarChar, 3000)
-						{ Value = "Hello!" };
+					SqlParameter contentParam = new SqlParameter("@vcontent", SqlDbType.NVarChar, 3000)
+						{ Value = "Howdy!" };
+					var parameters = new SqlParameter[] { titleParam, contentParam };
 
-					cmd.Parameters.AddRange(new SqlParameter[] { titleParam, contentParameter });
-					cmd.ExecuteNonQuery();
+				//cmd.Parameters.AddRange(new SqlParameter[] { titleParam, contentParameter });
+				//cmd.ExecuteNonQuery();
 
+				dbHelper.ExecuteNonQuery(sql, parameters);
 					Console.WriteLine("紀錄已新增");
 				}
 				catch (Exception ex)
@@ -42,7 +47,7 @@ namespace ConsoleApp3
 					Console.WriteLine($"操作失敗，原因:{ex.Message}");
 				}
 				#endregion
-			}
+			//}
 		}
 	}
 }
---
+++ iSpan.Utility/SqlDbHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iSpan.Utility
+{
+	public class SqlDbHelper
+	{
+		private string connString;
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="keyOfConnString">app.config裡的值，例如"default"</param>
+		public SqlDbHelper(string keyOfConnString)
+		{
+			connString = System.Configuration.ConfigurationManager.ConnectionStrings[keyOfConnString].ConnectionString;
+		}
+
+		public void ExecuteNonQuery(string sql, SqlParameter[] parameters)
+		{
+			using (var conn = new SqlConnection(connString))
+			{
+				
+					SqlCommand cmd = new SqlCommand(sql, conn);
+					conn.Open();
+
+
+					cmd.Parameters.AddRange(parameters);
+					cmd.ExecuteNonQuery();
+
+			}
+				
+			}
+		}
+}
